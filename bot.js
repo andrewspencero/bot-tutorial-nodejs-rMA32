ebe6abf3d036aa888e13dbde6702cde5c91617aa
andrewspencero <andrewospencer98@gmail.com>
2016-03-28 02:01:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,17 @@ function respond() {
     postMessage("o");
     this.res.end();
   } 
-
+  else if(request.text && botRegexHi.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("hey");
+    this.res.end();
+  }
+   else if(request.text && botRegexHello.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("hi");
+    this.res.end();
+   }
+   
    
   else {
     console.log("don't care");
