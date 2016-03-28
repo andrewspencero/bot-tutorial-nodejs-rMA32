0e64a737e81d6ef531d94afba2dfd3feb137784b
andrewspencero <andrewospencer98@gmail.com>
2016-03-28 02:24:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\sanders/;  botRegexDL = /^\/DDL/i;botRegexHi = /^\bernie/;botRegexHello = /^\/kale/
+      botRegex = /^\sanders/;  botRegexDL = /^\/DDL/i;botRegexHi = /^\bernie/;botRegexHello = /^\kale/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
